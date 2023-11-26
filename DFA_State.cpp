08c577f406581455d813494afbce54d027ffacf2
Ayman-Mohamed-abotaha <eng.ayman.mohamed.2001@gmail.com>
2023-11-26 09:59:47 +0200

    DFA_State

--- DFA_State.cpp
+++ DFA_State.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "DFA_State.h"
+
+void DFA_State::addNFAState(NFA_State *nfaState) {
+
+}
+
+DFA_State::DFA_State(bool isFinal) {
+
+}
+
+map<char, vector<NFA_State *>> DFA_State::getTransitions() {
+    return map<char, vector<NFA_State *>>();
+}
+
+bool DFA_State::isFinalState() {
+    return false;
+}
+
+void DFA_State::setFinalState(bool isFinal) {
+
+}
+
+DFA_State::~DFA_State() = default;
--- DFA_State.h
+++ DFA_State.h
@@ -6,9 +6,22 @@
 #define COMPILERS_DFA_STATE_H
 
 
+#include <set>
+#include "NFA_State.h"
+
 class DFA_State {
+private:
+    std::set<NFA_State*> nfaStates; // Set of NFA states in this DFA state
+    bool isFinal;
 
+public:
+    DFA_State( bool isFinal);
+    ~DFA_State();
+    map<char, vector<DFA_State*>> getTransitions();
+    bool isFinalState() ;
+    void setFinalState(bool isFinal);
+    void addNFAState(NFA_State* nfaState);//add nfa states which correspond to the dfa state
+    int id{};//?????
 };
-
 
 #endif //COMPILERS_DFA_STATE_H
